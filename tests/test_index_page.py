66870289dd7b24c86a076f667543b00d084a4da8
Evgeny <evgeny101292@mail.ru>
2023-04-15 15:45:14 +0300

    adjusted_test_name

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -6,14 +6,14 @@ case_3 = ['standard_user', 'qwerty', 'Epic sadface: Username and password do not
 case_4 = ['locked_out_user', 'secret_sauce', 'Epic sadface: Sorry, this user has been locked out']
 
 
-def test_successful(index_page):
+def test_successful_login(index_page):
     index_page.enter_username()
     index_page.enter_password(password='secret_sauce')
     index_page.click_to_login_btn()
     index_page.check_title()
 
 @pytest.mark.parametrize('username, password, expected_result', (case_1, case_2, case_3,case_4))
-def test_unsuccessful(index_page, username, password, expected_result):
+def test_unsuccessful_login(index_page, username, password, expected_result):
     index_page.enter_wrong_username(username)
     index_page.enter_password(password)
     index_page.click_to_login_btn()
