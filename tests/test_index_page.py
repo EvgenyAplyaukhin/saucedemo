6c99a68d2cc32c343341056140d78bc8fa78675b
unknown <you@example.com>
2023-05-14 07:35:21 +0300

    added checkout

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -44,22 +44,3 @@ def test_logout_login(index_page):
     index_page.click_to_login_btn()
     index_page.click_to_bm_burger_button()
     index_page.click_to_logout_sidebar_link()
-
-
-@allure.description('Place an order')
-@allure.label('owner', 'Jenya')
-@allure.title('By products')
-@allure.suite('Сheckout overview')
-@allure.severity(allure.severity_level.CRITICAL)
-def test_checkout_overview(index_page):
-    index_page.enter_username()
-    index_page.enter_password(password='secret_sauce')
-    index_page.click_to_login_btn()
-    index_page.click_to_add_to_card()
-    index_page.click_to_shopping_cart_link()
-    index_page.click_to_checkout()
-    index_page.enter_first_name()
-    index_page.enter_last_name()
-    index_page.enter_zip_postal_cod()
-    index_page.click_to_continue()
-    index_page.click_to_finish()
